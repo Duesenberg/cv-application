bc68857c8d456fed87a12a58a007440e8fb37139
Pande <popovski.pande@gmail.com>
2023-03-13 11:46:31 +0100

    Add initial state to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,59 @@ import Main from './components/Main';
 import './styles/App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      application: {
+        personalInfo: {
+          fullName: '',
+          address: '',
+          phoneNumber: '',
+          email: '',
+          dateOfBirth: ''
+        },
+        education: {
+          degree: {
+            name: '',
+            institution: '',
+            startYear: '',
+            endYear: ''
+          },
+          degrees: []
+        },
+        workExperience: {
+          experience: {
+            company: '',
+            position: '',
+            startYear: '',
+            endYear: '',
+            workDetail: '',
+            workDetails: []
+          },
+          experiences: []
+        },
+        skills: {
+          skill: '',
+          skills: []
+        },
+        projects: {
+          project: '',
+          projects: []
+        },
+        leadership: {
+          role: '',
+          roles: []
+        },
+        achievements: {
+          item: '',
+          list: []
+        }
+      },
+      applications: []
+    }
+  }
+  
   render() {
     return (
       <div className="screen">
