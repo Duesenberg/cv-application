ec878447386b1d44d1e482b099aa0f29a76d1aeb
Pande <popovski.pande@gmail.com>
2023-03-27 01:22:05 +0200

    Work on Education Section Edit feature
    Started working on the editing features of the education section.
    Wrote two methods:
      -one that changes state.editMode when the edit button is
      clicked,
      -and the other which changes state.degreeEdited to the id of the
      application.education.degrees array item in which the edit button
      will be clicked
    Depending on the value of these two properties, the corresponding
    HTML content will be generated.

--- src/components/DegreeList.js
+++ src/components/DegreeList.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 
 class DegreeList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editMode: false,
+      degreeEdited: null
+    }
+
+    this.toggleEditMode = this.toggleEditMode.bind(this);
+    this.setEditedDegree = this.setEditedDegree.bind(this);
+  }
+
+  toggleEditMode() {
+    this.state.editMode === false ?
+      this.setState({editMode: true}) :
+      this.setState({editMode: false});
+  }
+
+  setEditedDegree(e) {
+    const degreeEdited = e.target.dataset.id;
+    this.setState({degreeEdited: degreeEdited});
+  }
+
   render() {
     const { degrees } = this.props;
 
@@ -10,12 +33,23 @@ class DegreeList extends Component {
           <div
             key={degree.id}
             className='degree'>
-              <p className='text'>
-                {degree.name + ', ' + degree.institution + ', ' 
-                + degree.startYear + '-' + degree.endYear}
-              </p>
-              <button className='edit'>Edit</button>
-              <button className='remove'>Remove</button>
+              {(this.state.editMode === true && this.state.degreeEdited === degree.id) ?
+                <div className='edit-box'>
+                  <input type='text' name='fullName' id='fullName' required />
+                  + ', ' +
+                  <input type='text' name='institution' id='institution' required />
+                  + ', ' + 
+                  <input type='date' name='startYear' id='startYear' required />
+                  + '-' +
+                  <input type='date' name='endYear' id='endYear' required />
+                </div> :
+                <p className='text'>
+                  {degree.name + ', ' + degree.institution + ', ' 
+                  + degree.startYear + '-' + degree.endYear}
+                </p>
+                }
+              <button className='edit' onClick={this.toggleEditMode}>Edit</button>
+              <button data-id={degree.id} className='remove'>Remove</button>
           </div>    
         })}
       </div>
