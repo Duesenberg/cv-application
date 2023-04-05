21faaf845e2a5683a970c35c6df03681bab16538
Pande <popovski.pande@gmail.com>
2023-04-05 23:53:24 +0200

    Add style to education section
    Also made the information rows wider so more text would fit.

--- src/components/AddDegreePopUp.js
+++ src/components/AddDegreePopUp.js
@@ -10,19 +10,19 @@ class AddDegreePopUp extends Component {
     const { application, hidePopUp, addDegreeInfo, addDegree } = this.props;
 
     return (
-      <form className='add-degree' data-id={application.id} onSubmit={this.handleSubmit}>
+      <form className='degree-popup' data-id={application.id} onSubmit={this.handleSubmit}>
         <input 
           type='text' name='fullName' id='fullName' onChange={addDegreeInfo}
-          data-id={application.id} required />, 
+          data-id={application.id} placeholder='Degree' required />
         <input 
           type='text' name='institution' id='institution' onChange={addDegreeInfo} 
-          data-id={application.id} required />,  
+          data-id={application.id} placeholder='Institution' required />
         <input 
           type='date' name='startYear' id='startYear' onChange={addDegreeInfo} 
-          data-id={application.id} required />-
+          data-id={application.id} title='Year started' required />
         <input 
           type='date' name='endYear' id='endYear' onChange={addDegreeInfo} 
-          data-id={application.id} required />
+          data-id={application.id} title='Year finished' required />
         <button className='add-button' type='submit'>Add</button>
         <button className='close-button' onClick={hidePopUp}>Close</button>
       </form>
